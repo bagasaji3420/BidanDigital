2ca2a2a6825dc3aea27e3e05378e3c1e3871c7bd
bagasaji3420 <bagas.aji3420@gmail.com>
2026-05-16 17:30:34 +0700

    fix: rebuild vite assets

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,5 +23,9 @@ class AppServiceProvider extends ServiceProvider
         // Gate::define('viewLogViewer', function ($user) {
         //     return $user->hasRole('developer');
         // });
+
+        if (config('app.env') === 'production') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
