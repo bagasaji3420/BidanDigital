e7e6692ee21707df4ac68f29cb905713ecc3d489
bagasaji3420 <bagas.aji3420@gmail.com>
2026-05-16 17:18:01 +0700

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,17 +17,17 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'user.status' => \App\Http\Middleware\CheckUserStatus::class,
-            // 'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
-            // 'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
-            // 'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
 
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
 
-        // $exceptions->render(function (\Spatie\Permission\Exceptions\UnauthorizedException $e, $request) {
-        //     Alert::error('Denied', 'Access Denied');
+        $exceptions->render(function (\Spatie\Permission\Exceptions\UnauthorizedException $e, $request) {
+            Alert::error('Denied', 'Access Denied');
 
-        //     return redirect()->route('dashboard');
-        // });
+            return redirect()->route('dashboard');
+        });
     })->create();
